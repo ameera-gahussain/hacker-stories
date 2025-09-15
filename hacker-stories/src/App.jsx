788ae24d53b9dad628a8f4cd96c98b084f50f532
Ameera <ameera.gahussain@gmail.com>
2025-09-15 16:57:00 +0800

    lists in React

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -3,17 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <input id="search" type="text" />
+
+      <hr />
+      <ul>
+        {list.map(function (item) {
+        return (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title} </a>
+            <span> by {item.author} </span>
+            <span>{item.num_comments} comments </span>
+            <span>{item.points} points</span> 
+          </span>
+          </li>
+      );
+      })}
+      </ul>
     </div>
   );
 }
